d3b4517888d90102acba13dfe9ae77c5664422c4
HuyAngelDev <dev@vn.farm-angel.com>
2023-11-14 16:12:38 +0700

    update first time

---
+++ app/Http/Helpers.php
@@ -0,0 +1,45 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+if (!function_exists('uploaded_asset')) {
+    function uploaded_asset($id)
+    {
+        if (($asset = \App\Models\Uploads::find($id)) != null) {
+            return $asset->external_link == null ? my_asset($asset->file_name) : $asset->external_link;
+        }
+        return static_asset('assets/img/placeholder.jpg');
+    }
+}
+
+if (!function_exists('my_asset')) {
+    /**
+     * Generate an asset path for the application.
+     *
+     * @param string $path
+     * @param bool|null $secure
+     * @return string
+     */
+    function my_asset($path, $secure = null)
+    {
+        if (env('FILESYSTEM_DRIVER') == 's3') {
+            return Illuminate\Support\Facades\Storage::disk('s3')->url($path);
+        } else {
+            return app('url')->asset('public/' . $path, $secure);
+        }
+    }
+}
+
+if (!function_exists('static_asset')) {
+    /**
+     * Generate an asset path for the application.
+     *
+     * @param string $path
+     * @param bool|null $secure
+     * @return string
+     */
+    function static_asset($path, $secure = null)
+    {
+        return app('url')->asset($path, $secure);
+    }
+}
