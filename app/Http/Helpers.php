5035ea8b5046e20c99131099d58a0453345d891b
HuyAngelDev <dev@vn.farm-angel.com>
2023-11-14 16:24:47 +0700

    update new logic

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -8,7 +8,7 @@ if (!function_exists('uploaded_asset')) {
         if (($asset = \App\Models\Uploads::find($id)) != null) {
             return $asset->external_link == null ? my_asset($asset->file_name) : $asset->external_link;
         }
-        // return static_asset('assets/img/placeholder.jpg');
+        return static_asset('assets/img/placeholder.jpg');
     }
 }
 
