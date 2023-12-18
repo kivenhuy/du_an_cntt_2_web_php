4b8e31708043977decfdec7c8ce2a450724edd82
unknown <huyvonhat29101999@gmail.com>
2023-12-18 17:42:20 +0700

    huy võ update

---
+++ app/Http/Controllers/FarmManagement/CultivationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\FarmManagement;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cultivation;
+use Illuminate\Http\Request;
+
+class CultivationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cultivation $cultivation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cultivation $cultivation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cultivation $cultivation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cultivation $cultivation)
+    {
+        //
+    }
+}
