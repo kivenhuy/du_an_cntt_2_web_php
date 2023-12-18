c05f0b83c1b91f8d8e81f61066fcbb717c740272
unknown <huyvonhat29101999@gmail.com>
2023-12-18 17:46:00 +0700

    huy võ first update

--- database/migrations/2023_12_18_103636_create_cultivations_table.php
+++ database/migrations/2023_12_18_103636_create_cultivations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::connection('mysql_second')->create('cultivations', function (Blueprint $table) {
             $table->id();
             $table->integer('farmer_id');
+            $table->string('cultivation_name');
             $table->timestamps();
         });
     }
